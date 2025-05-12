55747c5bbb508d3cc471541ac88863eef076a25a
Jorge Alves <jorgeandrealves@gmail.com>
2025-05-12 17:36:22 +0100

    not working solution

---
+++ ai_agents/README.md
@@ -0,0 +1,3 @@
+
+> to solve crewai ChromaDB dependency please follow:
+https://community.crewai.com/t/nightmare-with-chroma-requires-sqlite3-3-35-0/1065/6
---
+++ ai_agents/src/ai_agents/crew.py
@@ -0,0 +1,56 @@
+# src/latest_ai_development/crew.py
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+
+llm = LLM(
+  model ="tinyllama",
+  base_url = "http://localhost:11434" 
+)
+
+@CrewBase
+class LatestAiDevelopmentCrew():
+  """LatestAiDevelopment crew"""
+
+  agents: List[BaseAgent]
+  tasks: List[Task]
+
+  @agent
+  def researcher(self) -> Agent:
+    return Agent(
+      config=self.agents_config['researcher'], # type: ignore[index]
+      verbose=True,
+      llm=llm,
+    )
+
+  @agent
+  def reporting_analyst(self) -> Agent:
+    return Agent(
+      config=self.agents_config['reporting_analyst'], # type: ignore[index]
+      llm=llm,
+      verbose=True
+    )
+
+  @task
+  def research_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['research_task'], # type: ignore[index]
+    )
+
+  @task
+  def reporting_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['reporting_task'], # type: ignore[index]
+      output_file='output/report.md' # This is the file that will be contain the final report.
+    )
+
+  @crew
+  def crew(self) -> Crew:
+    """Creates the LatestAiDevelopment crew"""
+    return Crew(
+      agents=self.agents, # Automatically created by the @agent decorator
+      tasks=self.tasks, # Automatically created by the @task decorator
+      process=Process.sequential,
+      verbose=True,
+    )
